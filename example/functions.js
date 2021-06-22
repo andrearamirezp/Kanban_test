6ad6dae051ff7ced231120dfb2f415a3e8572323
andrea <andreitaramirez94@gmail.com>
2021-06-22 12:40:32 -0400

    Primer commit

---
+++ example/functions.js
@@ -0,0 +1,15 @@
+function hacer_click(){
+    alert('Hicieron click en una tarjeta');
+}
+
+function click_derecho(){
+    alert('Hicieron click derecho en una tarjeta');
+}
+
+function me_arrastraron(){
+    alert('Arrastraste tarjeta');
+}
+
+function arrastrar_tablero(){
+    alert('Arrastrar tablero');
+}
